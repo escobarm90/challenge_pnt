d6f6ee1e1cd9d4643729b920b85d985349e60e05
macbookpro <m.escobar526@gmail.com>
2022-08-18 02:00:55 -0300

    no anda

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Repository.Repository;
 import entities.P_Almacen;
 import entities.P_Perfumeria;
 import entities.P_Verduleria;
@@ -5,16 +6,18 @@ import entities.P_Verduleria;
 public class Main {
     public static void main(String[] args) {
 
+        Repository repositorio = new Repository();
         P_Almacen producto = new P_Almacen("Coca-Cola zero",1.5,20);
         P_Almacen producto2 = new P_Almacen("Coca-Cola zero",1.5,20);
         P_Perfumeria producto3 = new P_Perfumeria("Shampoo Sedal",500,19);
         P_Verduleria producto4 = new P_Verduleria("Frutillas",54,"Kilos");
 
+        repositorio.guardarProductoEntity(producto);
 
 
-        System.out.println(producto.toString());
-        System.out.println(producto2.toString());
-        System.out.println(producto3.toString());
+        //System.out.println(producto.toString());
+        //System.out.println(producto2.toString());
+        //System.out.println(producto3.toString());
 
 
 
--- src/Repository/Repository.java
+++ src/Repository/Repository.java
@@ -8,9 +8,14 @@ import java.util.List;
 
 public class Repository {
 
-    List<Producto> lista = new ArrayList<Producto>();
+    List<Producto> lista = new ArrayList();
 
     public  void guardarProductoEntity(Producto producto){
         lista.add(producto);
+    }
+    public void leerdb(){
+        for(Producto item: lista){
+            toString();
+        }
     }
 }
